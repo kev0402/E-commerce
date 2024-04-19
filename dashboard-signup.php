2dbbca04458396d25b73506c13a1d55562fcc1aa
Kevin Balana <kevinnbalana@gmail.com>
2024-04-19 15:25:06 +0800

    kevin

--- dashboard-signup.php
+++ dashboard-signup.php
@@ -14,10 +14,41 @@
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
     <link rel="stylesheet" href="./bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./styles/dashboard.css">
 </head>
 <body>
-    <main>
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"></form>
-    </main>
+    <main class="d-flex flex-row w-100">
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" class="d-flex flex-column justify-content-center align-items-center mt-4 w-100">
+            <div class="personalFields w-50">
+                    <h1 class="fw-bold">JSK Store Dashboard</h1>
+                    <h2 class="mt-4">Sign Up</h2>
+                    <div class="error alert alert-danger"></div>
+                <div>
+                    <div class="d-flex mt-3 gap-5">
+                        <div>
+                            <label for="firstname" class="mb-2 fw-semibold">First Name</label>
+                            <input name="full-name" value="" type="text" class="form-control shadow-none p-3 fw-medium" id="firstname">
+                        </div>
+                        <div>
+                            <label for="lastname" class="mb-2 fw-semibold">Last Name</label>
+                            <input name="full-name" value="" type="text" class="form-control shadow-none p-3 fw-medium" id="lastname">
+                        </div>
+                    </div>
+                    <div class="mt-4">
+                        <label for="email" class="mb-2 fw-semibold">Email</label>
+                        <input name="email" value="" type="text" class="form-control shadow-none p-3 fw-medium" id="email">
+                    </div>
+                    <div class="mt-4">
+                        <label for="pass" class="mb-2 fw-semibold">Password</label>
+                        <input name="pass" value="" type="password" class="form-control shadow-none p-3 fw-medium" id="password">
+                    </div>
+                </div>
+                <button name="continueButton" class="btn btn-primary border border-0 rounded-pill w-100 mt-5 p-3" id="continueButton">Continue</button>
+                <p class="text-center fw-semibold link-offset-2 link-underline link-underline-opacity-0 mt-5">Already have an account? <a href="#" class="signup">Login</a></p>
+            </div>
+        </form>
+        <div>
+            <img src="./images/shoe.jpg" alt="" style="height: 100vh; border-top-left-radius: .75rem; border-bottom-left-radius: .75rem;">
+        </div>
 </body>
 </html>
