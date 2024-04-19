1d2fee27be3939c85eedaeeebe73fce4d0538143
DukeRaze <167498380+DukeRaze@users.noreply.github.com>
2024-04-19 16:08:56 +0800

    Add files via upload

---
+++ Dashboard.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/js/all.min.js"></script>
+    <script src="./JS/jquery.min.js"></script>
+    <script src="./JS/bootstrap.js"></script>
+    <script src="./JS/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="./CSS/bootstrap.min.css">
+    <link rel="stylesheet" href="./CSS/bootstrap.css">
+    <link rel="stylesheet" href="./CSS/Dashboard.css">
+    <title>Dashboard</title>
+</head>
+<body>
+
+<div class="DashboardContainer">
+
+<div class="Sidebar">
+
+<div class="NameLogo">
+
+<img src="./Image/Dashboard/logo.png" alt="">
+
+<label for="">JSK Store</label>
+
+</div>
+
+<img src="./Image/Dashboard/line breaker.png" alt="" class="linebreaker">
+
+<div class="SideBarIcons">
+
+<div class="IconName">
+
+<img src="./Image/Dashboard/DashboardIcon.png" alt="">
+
+<label for="">Dashboard</label>
+
+</div>
+
+<div class="IconName">
+
+<img src="./Image/Dashboard/ProductIcon.png" alt="">
+
+<label for="">Products</label>
+
+</div>
+
+<div class="IconName">
+
+<img src="./Image/Dashboard/OrderIcon.png" alt="">
+
+<label for="">Orders</label>
+
+</div>
+
+<div class="IconName">
+
+<img src="./Image/Dashboard/CustomerIcon.png" alt="">
+
+<label for="">Customers</label>
+
+</div>
+
+</div>
+
+</div>
+
+
+
+<div class="MainPage">
+
+<header>
+
+<div class="NavigationBar">
+
+<div class="SearchContainer">
+
+<input type="text" class="form-control" id="SearchInput">
+
+<i class="fa-solid fa-magnifying-glass" id="SearchButton"></i>
+
+</div>
+
+<div class="NavigationBarIcons">
+
+<img src="./Image/Dashboard/NotificationIcon.png" alt="">    
+
+<img src="./Image/Dashboard/MessageIcon.png" alt="">
+
+<div class="AdminAccount">
+
+<label for="">The Goat</label>
+
+<img src="./Image/Reviewer IMG/1641386961385.jpg" alt="">
+
+</div>
+
+</div>
+
+</div>
+
+</header>
+
+</div>
+
+</div>
+
+</body>
+
+<script type="text/javascript" src="./JS/Dashboard.js"></script>
+
+</html>
