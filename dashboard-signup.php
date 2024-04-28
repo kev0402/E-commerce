eb4bda0781357e38a3e9136a3d094d257467c862
kev0402 <kevinnbalana@gmail.com>
2024-04-28 18:46:26 +0800

    small changes: Change title of the tab in the dashboard-signup

--- dashboard-signup.php
+++ dashboard-signup.php
@@ -7,7 +7,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard Sign-Up</title>
+    <title>Creating an Account</title>
     <link rel="icon" href="./images/logo.png">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
